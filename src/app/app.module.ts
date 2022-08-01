6a8427ca5b82148b4d7fe4e8bf542f9eab3b4bc4
Mahfuz-Zaman <zaman.13121996@gmail.com>
2022-08-01 11:02:54 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { BottomMostFooterComponent } from './bottom-most-footer/bottom-most-footer.component';
+import { FooterMainComponent } from './footer-main/footer-main.component';
+import { MailListComponent } from './mail-list/mail-list.component';
+import { DestinationComponent } from './destination/destination.component';
+import { DiscountComponent } from './discount/discount.component';
+import { OffersComponent } from './offers/offers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    BottomMostFooterComponent,
+    FooterMainComponent,
+    MailListComponent,
+    DestinationComponent,
+    DiscountComponent,
+    OffersComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
